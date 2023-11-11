d6719286f965f32ddcc4fb4ee4e8e88af405550a
DipakG9 <goredipak999@gmail.com>
2023-11-11 21:49:40 +0530

    created state table

--- Backend/Database/SwapKartDB.sql
+++ Backend/Database/SwapKartDB.sql
@@ -6,6 +6,7 @@
 
 drop database if exists swapkart;
 create database swapkart;
+show databases;
 
 /*
     Date: 09/11/2023
@@ -24,3 +25,40 @@ create database swapkart;
     Author: Shreyas Sonawane
     Purpose: testing git
 */
+use swapkart;
+create table state (state_id int primary key,state_name varchar(25));
+insert into state values(01,'Andhra Pradesh');
+insert into state values(02,'Arunachal Pradesh');
+insert into state values(03,'Assam'),(04,'Bihar'),
+(05,'Chhattisgarh'),
+(06,'Goa'),
+(07,'Gujarat'),
+(08,'Haryana'),
+(09,'Himachal Pradesh'),
+(10,'Jammu and Kashmir'),
+(11,'Jharkhand '),
+(12,'Jharkhand '),
+(13,'Kerala '),
+(14,'Madhya Pradesh'),
+(15,'Maharashtra'),
+(16,'Manipur'),
+(17,'Meghalaya'),
+(18,'Mizoram'),
+(19,'Nagaland'),
+(20,'Odisha'),
+(21,'Punjab'),
+(22,'Rajasthan'),
+(23,'Sikkim '),
+(24,'Tamil Nadu '),
+(25,'Telangana '),
+(26,'Tripura '),
+(27,'Uttar Pradesh'),
+(28,'Uttarakhand'),
+(29,'West Bengal');
+SELECT*FROM state;
+
+
+
+
+
+
