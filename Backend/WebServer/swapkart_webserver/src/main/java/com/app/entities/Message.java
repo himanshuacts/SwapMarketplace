9226d324046eb31097d76f601acd9f8ed1aad898
himanshuacts <himanshu.wakodikar.cdac@gmail.com>
2024-02-04 16:55:39 +0530

    modified message and transaction history entity

--- Backend/WebServer/swapkart_webserver/src/main/java/com/app/entities/Message.java
+++ Backend/WebServer/swapkart_webserver/src/main/java/com/app/entities/Message.java
@@ -3,28 +3,45 @@ package com.app.entities;
 import java.time.LocalDate;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "chatbox")
 public class Message {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "msg_id")
 	private int msgId;
 
-	@ManyToOne
+	@OneToOne
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	@ManyToOne
+	@OneToOne
 	@JoinColumn(name = "owner_id")
 	private User owner;
 
-	private Product userProduct; // Assuming Product object
-	private Product ownerProduct; // Assuming Product object
+	@Column(name = "user_product_id")
+	private int userProductId;
+	
+	@Column(name = "owner_product_id")
+	private int ownerProductId;
 
 	@Column(name = "role", columnDefinition = "VARCHAR(20)")
 	private String role;
--- Backend/WebServer/swapkart_webserver/src/main/java/com/app/entities/TransactionHistory.java
+++ Backend/WebServer/swapkart_webserver/src/main/java/com/app/entities/TransactionHistory.java
@@ -1,5 +1,13 @@
 package com.app.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -7,14 +15,32 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "transaction_history")
 public class TransactionHistory {
-	private User user;
-	private Product userProduct; // Assuming Product object
-	private User owner;
-	private Product ownerProduct; // Assuming Product object
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "transaction_history_id")
+	private int transactionHistoryId;
+	
+	@OneToOne
+	@JoinColumn(name = "transaction_id")
+	@MapsId("transactionId")
+	private Transaction transactionId;
+	
+	@Column(name = "user_id")
+	private int userId;
+	
+	@Column(name = "user_product_id")
+	private int userProductId; // Assuming Product object
+	
+	@Column(name = "owner_id")
+	private int ownerId;
+	
+	@Column(name = "owner_product_id")
+	private int ownerProductId; // Assuming Product object
 }
