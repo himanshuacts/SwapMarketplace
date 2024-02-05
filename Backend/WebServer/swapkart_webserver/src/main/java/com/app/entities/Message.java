8192628b692aacea1f7c6e5ffeeb984417ac4491
himanshuacts <himanshu.wakodikar.cdac@gmail.com>
2024-02-05 11:47:29 +0530

    message entity updated

--- Backend/WebServer/swapkart_webserver/src/main/java/com/app/entities/Message.java
+++ Backend/WebServer/swapkart_webserver/src/main/java/com/app/entities/Message.java
@@ -15,6 +15,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 
 @Entity
@@ -22,6 +23,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Table(name = "chatbox")
 public class Message {
 	@Id
