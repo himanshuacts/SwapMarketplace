5b147bb0a34dafbb3f11b6c1c1a4e908711fadb1
Arsharaj Chauhan <arsharajchauhan343@gmail.com>
2024-02-06 23:11:48 +0530

    Added UserRepository to com.app.dao package.

---
+++ Backend/WebServer/swapkart_webserver/src/main/java/com/app/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.app.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
