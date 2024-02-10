5f3be4a995e58a9e169edbaf6cd8acedd0244fa2
DipakG9 <goredipak999@gmail.com>
2024-02-10 22:11:03 +0530

    Added WishController and WishService with DTOs

---
+++ Backend/WebServer/swapkart_webserver/src/main/java/com/app/dto/AddWishReqDTO.java
@@ -0,0 +1,17 @@
+package com.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class AddWishReqDTO {
+	private Integer userId;
+	private Integer productId;
+}
